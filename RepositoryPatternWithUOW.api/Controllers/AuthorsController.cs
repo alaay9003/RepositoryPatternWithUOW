855daede6bc84108097d1c6d06c64946a306e073
alaay9003 <alaay9003@gmail.com>
2023-03-31 02:24:16 +0200

    Add project files.

---
+++ RepositoryPatternWithUOW.api/Controllers/AuthorsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RepositoryPatternWithUOW.Core.Interfaces;
+using RepositoryPatternWithUOW.Core.Models;
+
+namespace RepositoryPatternWithUOW.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly IBaseRepository<Author> _authorRepository;
+
+        public AuthorsController(IBaseRepository<Author> authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+        [HttpGet]
+        public IActionResult GetById()
+        {
+            return Ok(_authorRepository.GetById(1));
+        }
+
+    }
+}
